faa33473c1795a566d8120e60a4d377ff544db8d
Aayush Kapoor <xeoneux@icloud.com>
2020-04-11 12:14:34 +0530

    Run simple actix server that prints hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("localhost:8080")?
+        .run()
+}
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
 }
